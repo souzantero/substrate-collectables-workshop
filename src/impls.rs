ce6832d77c0acd35acba17b7232d4425a923b928
Felipe Antero <souzantero@gmail.com>
2024-12-02 16:29:42 -0300

    feat: create kitty adds to map

--- src/impls.rs
+++ src/impls.rs
@@ -2,10 +2,11 @@ use super::*;
 use frame::prelude::*;
 
 impl<T: Config> Pallet<T> {
-	pub fn mint(owner: T::AccountId) -> DispatchResult {
+	pub fn mint(owner: T::AccountId, dna: [u8; 32]) -> DispatchResult {
 		let current_count = CountForKitties::<T>::get();
 		let new_count = current_count.checked_add(1).ok_or(Error::<T>::TooManyKitties)?;
 		CountForKitties::<T>::set(new_count);
+		Kitties::<T>::insert(dna, ());
 		Self::deposit_event(Event::<T>::Created { owner });
 		Ok(())
 	}
